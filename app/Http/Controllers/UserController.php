83e6da6602a138b001f1c38129b5a0cbcf3565e2
Ginel Exaltado <ginelexaltado@gmail.com>
2017-01-03 01:02:22 +0800

    Videos 10-22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
-
 class UserController extends Controller
 {
-	public function getDashboard()
-	{
-		return view('dashboard');
-	}
-
 	public function postSignUp(Request $request)
 	{
 		$this -> validate($request, [
@@ -20,8 +14,6 @@ class UserController extends Controller
 			'first_name' => 'required|max:120',
 			'password' => 'required|min:4'
 		]);
-
-		
 		$email = $request['email'];
 		$first_name = $request['first_name'];
 		$password = bcrypt($request['password']);
@@ -33,19 +25,45 @@ class UserController extends Controller
 		$user->save();
 		Auth::login($user);
 		return redirect()->route('dashboard');
-		
 	}
 	public function postSignIn(Request $request)
 	{
 		$this -> validate($request, [
-			'email' => 'required|email',
+			'email' => 'required',
 			'password' => 'required'
 		]);
-		
 		if ( Auth::attempt( ['email' => $request['email'], 'password' => $request['password'] ] ) ) {
 			return redirect()->route('dashboard');
 		}
 		return redirect()->back();
 	}
-	
+	public function getLogout()
+	{
+		Auth::logout();
+		return redirect()->route('home');
+	}
+	public function getAccount()
+	{
+		return view('account', ['user' => Auth::user()]);
+	}
+	public function postSaveAccount(Request $request)
+	{
+		$this->validate($request, [
+			'first_name' => 'required|max:120'
+		]);
+		$user = Auth::user();
+		$user->first_name = $request['first_name'];
+		$user->update();
+		$file = $request->file('image');
+		$filename = $request['first_name'] . '-' . $user->id . '.jpg';
+		if($file) {
+			Storage::disk('local')->put($filename, File::get($file));
+		}
+		return redirect()->route('account');
+	}
+	public function getUserImage($filename)
+	{
+		$file = Storage::disk('local')->get($filename);
+		return new Response($file, 200);
+	}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,4 +41,40 @@
 
 
     			]);
+    		Route::get('/logout', [
+		'uses' => 'UserController@getLogout',
+		'as' => 'logout'
+	]);
+	Route::get('/account', [
+		'uses' => 'UserController@getAccount',
+		'as' => 'account'
+	]);
+	Route::post('/updateaccount', [
+		'uses' => 'UserControlller@postSaveAccount',
+		'as' => 'account.save'
+	]);
+	Route::get('/userimage/{filename}', [
+		'uses' => 'UserController@getUserImage',
+		'as' => 'account.image'
+	]);
+	Route::get('/dashboard', [
+		'uses' => 'PostController@getDashboard',
+		'as' => 'dashboard',
+		'middleware' => 'auth'
+	]);
+	
+	Route::get('/delete-post/{post_id}', [
+		'uses' => 'PostController@getDeletePost',
+		'as' => 'post.delete',
+		'middleware' => 'auth'
+	]);
+	Route::post('/edit', [
+		'uses' => 'PostController@postEditPost',
+		'as' => 'edit'
+	]);
+	Route::post('/like', [
+		'uses' => 'PostController@getLikePost',
+		'as' => 'like'
+	]);
+
 });
